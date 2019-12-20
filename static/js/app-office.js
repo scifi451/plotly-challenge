c0916aa879054a1c9593eb616c518a71af9c2702
Matt Pollari <mdpollari@gmail.com>
2019-12-20 11:59:25 -0600

    Homework working on all three. Cleanup still needed

--- static/js/app-office.js
+++ static/js/app-office.js
@@ -29,25 +29,94 @@ function DrawBargraph(desiredSampleID)
     };
 
     Plotly.newPlot("bar", barData, barLayout);
+
+    var trace1 = {
+        x: otu_ids,
+        y: sample_values,
+        mode: 'markers',
+        text: otu_labels,
+        marker: {
+            color: otu_ids,
+            size: sample_values, 
+            colorscale: 'Earth'
+        }
+
+    };
+    var data = [trace1];
+
+    var layout = {
+        title: 'Bacteria Cultures',
+        xaxis: { title: "OTU ID's" },
+        yaxis: { title: "Number of Bacteria" },
+
+    }
+
+    Plotly.newPlot ('bubble',data, layout)
 });
 
 }
 
-function DrawBubbleChart(sampleID)
-{
-    console.log("DrawBargraph: sample = ", sampleID);    
-}
+// function DrawBubbleChart(desiredSampleID)
+// {
+//     console.log("DrawBubbleChart: sample = ", desiredSampleID);    
 
-function ShowMetadata(sampleID)
+//     d3.json("static/data/samples.json").then((data) => {
+
+//         var samples = data.samples;
+//         var resultArray = samples.filter(sampleObj => sampleObj.id == desiredSampleID);
+//         var result = resultArray[0];
+
+//         var otu_ids = result.otu_ids;
+//         var otu_labels = result.otu_labels;
+//         var sample_values = result.sample_values;
+   
+//         var trace1 = [
+//             {
+//                 x: [otu_ids],
+//                 y: [sample_values], 
+//                 mode: 'markers',
+//                 marker: {
+//                    size: [sample_values],
+//                    color: [otu_ids],
+//                 }
+//                 text: [otu_labels]
+//             }
+//         ];
+//         var bubbleData = [trace1]
+
+//     var bubbleLayout = {
+//         title: "Top 10 Bacteria Cultures Found",
+//         margin: {t: 30, 1: 150}
+//     };
+
+//     Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+// }); 
+// }
+
+function ShowMetadata(desiredSampleID)
 {
-    console.log("ShowMetadata: sample = ", sampleID);
+    console.log("ShowMetadata: sample = ", desiredSampleID);
+    d3.json("static/data/samples.json").then((data) => {
+        var metadata = data.metadata;
+        // Filter the data for the object with the desired sample number
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == desiredSampleID);
+        var result = resultArray[0];  
+        // Use d3 to select the panel with id of `#sample-metadata`
+        var panel = d3.select("#sample-metadata");
+         // Use `.html("") to clear any existing metadata
+        panel. html('');
+           // Use `Object.entries` to add each key and value pair to the panel
+        Object.entries(result).forEach(([key, value]) => 
+        {      panel.append("h6").text(`${key.toUpperCase()}: ${value}`); 
+       });  
+    });
 }
 
 function optionChanged(newSampleID)
 {
     console.log("Dropdown changed to: ", newSampleID)
     DrawBargraph(newSampleID);
-    DrawBubbleChart(newSampleID);
+    // DrawBubbleChart(newSampleID);
     ShowMetadata(newSampleID);
 }
 
@@ -69,9 +138,9 @@ function Init ()
         });
     });
 
-
+    
     DrawBargraph(sampleID);
-    DrawBubbleChart(sampleID);
+    // DrawBubbleChart(sampleID);
     ShowMetadata(sampleID);
 }
 
